c4e9b660c4092a9ae9b7f351fdbdae221509e4dc
Andre Nascimento <1914andre@gmail.com>
2024-10-14 14:47:18 -0300

    subindo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import React from "react"
+import LoginPage from "./components/login/login"
+
+
+
+function App() {
+  
+  return (
+    <>
+   <section>
+      <LoginPage/>
+   </section>
+    </>
+  )
+}
+
+export default App
